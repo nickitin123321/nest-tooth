ac28442dd1c29583cea79abc7a6e1b5c7c623b14
Ilya <irracli@gmail.com>
2022-10-31 02:45:54 +0400

    fix unused

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { genSalt, hash } from 'bcryptjs';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
@@ -9,15 +8,5 @@ export class UsersService {
 
   async findOne(email: string): Promise<User> {
     return this.prismaService.user.findUniqueOrThrow({ where: { email } });
-  }
-
-  async createOne(email: string, password: string) {
-    const passwordHash = await this.hashPassword(password);
-    this.prismaService.user.create({ data: { email, passwordHash } });
-  }
-
-  private async hashPassword(password: string): Promise<string> {
-    const salt = await genSalt(10);
-    return await hash(password, salt);
   }
 }
